9795c1c3f08b87554e1c8cff3f7b38ec7d48561d
Akmal Kamarudin <akmal.kamarudin19@gmail.com>
2023-05-25 21:49:28 +0800

    chore: solve quiz7 and quiz8

---
+++ quiz7.js
@@ -0,0 +1,14 @@
+// Write a function called sumOfSquares that takes an array of integers as an argument and returns the sum of the squares of those integers.
+// For example, if the input is [1, 2, 3], the output should be 14 (i.e. 1^2 + 2^2 + 3^2 = 14).
+
+function sumOfSquares(nums) {
+  // Your code here
+}
+
+const input1 = [1, 2, 3];
+// const output1 = 14;
+sumOfSquares(input1);
+
+const input3 = [-1, 2, -3, 4];
+// const output3 = 30;
+sumOfSquares(input3);
---
+++ quiz8.js
@@ -0,0 +1,14 @@
+// Write a function called maxConsecutiveSum that takes an array of integers as an argument and returns the maximum sum of any consecutive subarray of the input array.
+// For example, if the input is [1, -2, 3, 10, -4, 7, 2, -5], the output should be 18 (i.e. the sum of the subarray [3, 10, -4, 7, 2]).
+
+function maxConsecutiveSum(nums) {
+  // Your code here
+}
+
+const input1 = [1, -2, 3, 10, -4, 7, 2, -5];
+// const output1 = 18;
+maxConsecutiveSum(input1);
+
+input2 = [-2, -1, -3, -4, -1];
+const output2 = -1;
+maxConsecutiveSum(input2);
