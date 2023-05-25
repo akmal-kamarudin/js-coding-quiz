e43bc0c7c03e6d8b389fc74faa60966cdd9cc233
Akmal Kamarudin <akmal.kamarudin19@gmail.com>
2023-05-25 15:32:50 +0800

    fix: quiz 5 solved. made a pattern for comparing and group them together inside a nested array

--- quiz5.js
+++ quiz5.js
@@ -4,14 +4,51 @@
 
 function groupAnagrams(strs) {
   // Your code here
+  let sortArr = [];
   for (let i = 0; i < strs.length; i++) {
-    let word = strs[i].split("");
-    let sortWord = word.sort();
-
-    console.log(sortWord);
+    let word1 = strs[i].split("");
+    let sortWord1 = word1.sort();
+    sortArr.push(sortWord1.join(""));
   }
+
+  let arrPattern = [];
+  for (let i = 0; i < sortArr.length; i++) {
+    for (let j = 0; j < sortArr.length; j++) {
+      if (sortArr[i] !== sortArr[j]) {
+        if (arrPattern.indexOf(sortArr[i]) === -1) {
+          arrPattern.push(sortArr[i]);
+        }
+      } else {
+        if (arrPattern.indexOf(sortArr[i]) === -1) {
+          arrPattern.push(sortArr[i]);
+        }
+      }
+    }
+  }
+
+  let tempArr = [];
+  let outputArr = [];
+
+  for (let j = 0; j < arrPattern.length; j++) {
+    for (let k = 0; k < sortArr.length; k++) {
+      if (arrPattern[j] === sortArr[k]) {
+        if (tempArr.indexOf(strs[k]) === -1) {
+          tempArr.push(strs[k]);
+        }
+      }
+    }
+    outputArr.push(tempArr);
+    tempArr = [];
+  }
+
+  console.log(outputArr);
+  return outputArr;
 }
 
 const input1 = ["eat", "tea", "tan", "ate", "nat", "bat"];
 // const output1 = [["eat", "tea", "ate"], ["tan", "nat"], ["bat"]];
 groupAnagrams(input1);
+
+const input2 = ["listen", "silent", "enlist", "inlets", "linset"];
+// const output2 = [["listen", "silent", "enlist", "inlets", "linset"]];
+groupAnagrams(input2);
