8e74e3b36a29b679254907f00a62573424bee634
Akmal Kamarudin <akmal.kamarudin19@gmail.com>
2023-05-29 16:53:21 +0800

    chore: rework the solution to cater negative nums

--- quiz8.js
+++ quiz8.js
@@ -3,12 +3,43 @@
 
 function maxConsecutiveSum(nums) {
   // Your code here
+  let max = nums[0],
+    min = nums[0];
+
+  for (let i = 0; i < nums.length; i++) {
+    let value = nums[i];
+
+    if (min > value) {
+      min = value;
+    } else if (max < value) {
+      max = value;
+    }
+  }
+
+  let maxSum = 0,
+    maxCurrent = 0;
+
+  for (let j = 0; j < nums.length; j++) {
+    maxCurrent = maxCurrent + nums[j];
+    console.log(maxCurrent);
+
+    if (maxSum < maxCurrent) {
+      maxSum = maxCurrent;
+    } else if (maxCurrent < 0) {
+      maxCurrent = 0;
+    } else {
+      maxSum = maxCurrent;
+    }
+  }
+
+  console.log(maxSum);
+  return maxSum;
 }
 
 const input1 = [1, -2, 3, 10, -4, 7, 2, -5];
 // const output1 = 18;
-maxConsecutiveSum(input1);
+// maxConsecutiveSum(input1);
 
-input2 = [-2, -1, -3, -4, -1];
-const output2 = -1;
+const input2 = [-2, -1, -3, -4, -1];
+// const output2 = -1;
 maxConsecutiveSum(input2);
