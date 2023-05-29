dca21078ae51dd4074888aea5598e9e2f3a4f133
Akmal Kamarudin <akmal.kamarudin19@gmail.com>
2023-05-29 18:56:25 +0800

    fix: solve quiz8, handled negative sums in array

--- quiz8.js
+++ quiz8.js
@@ -3,32 +3,19 @@
 
 function maxConsecutiveSum(nums) {
   // Your code here
-  let max = nums[0],
-    min = nums[0];
-
-  for (let i = 0; i < nums.length; i++) {
-    let value = nums[i];
-
-    if (min > value) {
-      min = value;
-    } else if (max < value) {
-      max = value;
-    }
-  }
-
   let maxSum = 0,
     maxCurrent = 0;
 
-  for (let j = 0; j < nums.length; j++) {
+  for (let j = 0; j <= nums.length; j++) {
     maxCurrent = maxCurrent + nums[j];
-    console.log(maxCurrent);
 
     if (maxSum < maxCurrent) {
       maxSum = maxCurrent;
+    } else if (maxSum === 0) {
+      maxSum = maxCurrent;
+      maxCurrent = 0;
     } else if (maxCurrent < 0) {
       maxCurrent = 0;
-    } else {
-      maxSum = maxCurrent;
     }
   }
 
@@ -38,8 +25,16 @@ function maxConsecutiveSum(nums) {
 
 const input1 = [1, -2, 3, 10, -4, 7, 2, -5];
 // const output1 = 18;
-// maxConsecutiveSum(input1);
+maxConsecutiveSum(input1);
 
 const input2 = [-2, -1, -3, -4, -1];
 // const output2 = -1;
 maxConsecutiveSum(input2);
+
+const input4 = [1, -1, 2, -2, 3, -3];
+// const output4 = 3;
+maxConsecutiveSum(input4);
+
+const input5 = [];
+// const output5 = NaN;
+maxConsecutiveSum(input5);
